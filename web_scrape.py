3f9f17e4a483b5a86fca581154ae33f78ab2eebf
realendie <enderproplays54@gmail.com>
2024-12-30 21:21:31 -0500

    Enhance web scraping in web_scrape.py to include package date and description

--- web_scrape.py
+++ web_scrape.py
@@ -2,8 +2,10 @@ from bs4 import BeautifulSoup
 import requests as re
 import requests
 
-html_text = requests.get('https://pypi.org/project/RandomPi/').text
-soup = BeautifulSoup(html_text, features='html.parser')
-name = soup.find('h1', class_ = 'package-header__name').text
+html_text = requests.get("https://pypi.org/project/RandomPi/").text
+soup = BeautifulSoup(html_text, features="html.parser")
+name = soup.find("h1", class_="package-header__name").text.strip()
+date = soup.find("p", class_="package-header__date").text
+des = soup.find("p", class_="package-description__summary").text
 
-print(name)
+print(f'Name:\n  {name}{date}Description:\n  {des}')
