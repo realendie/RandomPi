bae04fc438576ee0f7312e82795e03535d33a3da
realendie <enderproplays54@gmail.com>
2024-12-31 12:39:07 -0500

    web_scrape corrrect syntax not working

--- web_scrape.py
+++ web_scrape.py
@@ -2,10 +2,12 @@ from bs4 import BeautifulSoup
 import requests as re
 import requests
 
-html_text = requests.get("https://pypi.org/project/RandomPi/").text
+html_text = requests.get(
+    "https://pypi.org/project/affinidi_tdk_credential_verification_client/"
+).text
 soup = BeautifulSoup(html_text, features="html.parser")
 name = soup.find("h1", class_="package-header__name").text.strip()
 date = soup.find("p", class_="package-header__date").text
 des = soup.find("p", class_="package-description__summary").text
 
-print(f'Name:\n  {name}{date}Description:\n  {des}')
+print(f"Name:\n  {name}{date}Description:\n  {des}\n")
